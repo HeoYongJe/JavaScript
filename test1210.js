23716ab0831a4fbf14326f1660b3d9c4f336f4c1
HeoYongJe <hyj.campus@gmail.com>
2020-12-10 23:41:32 +0900

    rest and spread

---
+++ test1210.js
@@ -0,0 +1,54 @@
+function square(x){
+    return x * x ;
+}
+let s = square;
+console.log(s)
+console.log(square(4))
+console.log(s(4))
+
+//객체 안
+
+let o = {
+    square: function(x){
+        return x * x;
+    }
+}
+console.log(o.square(4));
+
+let arr = [
+    x => x * x, 20
+];
+
+let arr2 = arr[0];
+console.log(arr[1])
+
+let scpoe = "global";
+function checkscope(s){
+    let scope = "local";
+    function f() {return s + " " + " " + scope; }
+    return f
+}
+let f = checkscope()();
+console.log(f)
+
+
+//rest  -> 나머지 arguments를 배열로 
+const add = (a,b, ...rest) => {
+    console.log(a); // 1
+    console.log(b); // 2
+    console.log(rest); // [5,7,4,22,4]
+}
+add(1,2,5,7,4,22,4);
+
+
+//spread 배열 문자열 등의 이터러블을 분해해서 개별 요소로 만듬
+
+const spr = [1,2,3];
+console.log(spr);
+console.log(...spr) // 1 2 3
+
+const spr2 = [4,5,6];
+const spr3 = '7';
+console.log(...spr, ...spr2, ...spr3) // 1 2 3 4 5 6 '7'
+
+
