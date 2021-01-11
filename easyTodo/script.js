693964b1fae8511769d3d67154fb83f8c85aea3f
HeoYongJe <hyj.campus@gmail.com>
2021-01-11 15:57:12 +0900

    basic todo

---
+++ easyTodo/script.js
@@ -0,0 +1,27 @@
+let $button = document.getElementById('button');
+let $input = document.getElementById('input');
+let $list = document.getElementById('list');
+
+$button.addEventListener('click', clickButton);
+let cnt = 1;
+
+// function clickButton(){
+//     let temp = document.createElement('li');
+//     temp.innerHTML = input.value;
+//     list.appendChild(temp);
+// } //clickButton 함수는 클릭하면 li 요소를 만들어서 input 데이터를 넣고 list에 추가하는 함수
+
+function clickButton(){
+    let temp = document.createElement('li');
+    temp.setAttribute("class", "listItem")
+    temp.setAttribute("id", "li" + cnt);
+    temp.innerHTML = input.value;
+    temp.innerHTML += "<button class='del' type='button'onclick='remove("+cnt+")'>삭제</button>";
+    list.appendChild(temp);
+    cnt++;
+}
+
+function remove(cnt) {
+    var li = document.getElementById('li'+cnt);
+    list.removeChild(li);
+  }
