01d94e8dc52f75d8e3fcae0e4886c17eeccd60a5
HeoYongJe <hyj.campus@gmail.com>
2021-01-13 23:31:15 +0900

    삭제기능 작업중

--- easyTodo/script.js
+++ easyTodo/script.js
@@ -1,32 +1,48 @@
 let $button = document.getElementById('button');
 let $input = document.getElementById('input');
 let $list = document.getElementById('list');
+// let $checkBtn = document.getElementsByClassName('checkBtn')
+
+
 
 $button.addEventListener('click', clickButton);
 let cnt = 1;
-let checkBtn = [];
+// let checkBtn = [];
 // function clickButton(){
 //     let temp = document.createElement('li');
 //     temp.innerHTML = input.value;
 //     list.appendChild(temp);
 // } //clickButton 함수는 클릭하면 li 요소를 만들어서 input 데이터를 넣고 list에 추가하는 함수
 
-function clickButton(){
+function clickButton() {
     let temp = document.createElement('li');
     temp.setAttribute("class", "listItem")
     temp.setAttribute("id", "li" + cnt);
     temp.innerHTML = input.value;
-    temp.innerHTML += "<button class='del' type='button'onclick='remove("+cnt+")'>삭제</button>";
+    temp.innerHTML += "<button class='del' type='button'onclick='remove(" + cnt + ")'>삭제</button>";
     temp.innerHTML += "<input class='checkBtn' type='checkbox'></input>"
     list.appendChild(temp);
     cnt++;
 }
 
-function remove(cnt) {
-    var li = document.getElementById('li'+cnt);
-    if(checkBtn.ckecked == true) {
-        list.removeChild(li);
-    } else {
-        alert('체크해주세요');
+
+input.addEventListener('click', clickCheck);
+
+function clickCheck() {
+    if($checkBtn.checked == 1){
+        $checkBtn.classList.add('checked');
+    } else{
+        $checkBtn.remove('checked');
     }
-  }
+}
+
+function remove(cnt) {
+    var li = document.getElementById('li' + cnt);
+    var checkBtn = document.getElementsByClassName('checked');
+        if (checkBtn.classList(checked) != -1) {
+            list.removeChild(li);
+        } else {
+            alert('돌아가');
+        }
+
+}
