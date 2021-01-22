c70b6894f9eb2927702ddfea7f096a0e64beb335
HeoYongJe <hyj.campus@gmail.com>
2021-01-22 23:57:04 +0900

    내용추가

--- node.js
+++ node.js
@@ -6,3 +6,8 @@ const { divide } = require("lodash");
 document.prototype.getElementById 메소드는 인수로 전달한 id 어트리뷰트 값을 갖는 하나의 요소노드를 탐색하여 반환
 getElementById 메소드는 인수로 전달된 id 값을 갖는 첫번째 요소 노트만을 반환
 즉, 언제나 단 하나의 요소노드를 반환한다
+
+
+document.prototype.quertSelector 메소드는 인수로 전달한 CSS선택자를 만족시키는 하나의 요소 노드를 탐색하여 반환한다
+노드가 여러개인 경우, 첫번째 요소 노드만 반환한다
+만족시키는 요소고 없을 경우 null을 반환
